3e2d96a5eb804a56f5d315a2b81c7d8b658be7d6
Andrew Roldugin <andrew.roldugin@gmail.com>
2018-11-01 18:31:06 +0300

    Remove namespace in file_test.cc to avoid indentation for tests

--- src/lsd/file_test.cc
+++ src/lsd/file_test.cc
@@ -2,9 +2,9 @@
 
 #include "lsd/file.h"
 
-namespace lsd {
+using namespace lsd;
+
 TEST(ReadTextTest, ReadFileToString) {
   std::string expected = "one\ntwo\nthree\n";
   EXPECT_EQ(expected, ReadText("data/read_text/file.txt"));
-}
 }
